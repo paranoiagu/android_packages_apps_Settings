98d6c08c299e4c39d296252e0d4cc4c823377d12
paranoiagu <gu.yanfeng@gmail.com>
2016-06-01 15:10:43 +0800

    [1/2]add headset icon, su indicator, 4g/lte 3g/h+ show

--- src/com/android/settings/pd/PdSettings.java
+++ src/com/android/settings/pd/PdSettings.java
@@ -17,40 +17,97 @@
 package com.android.settings.pd;
 
 import android.content.Context;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.Preference;
+import android.preference.SwitchPreference;
 import android.preference.PreferenceScreen;
 import android.provider.SearchIndexableResource;
+import android.provider.Settings;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
+import com.android.internal.util.pd.PDUtils;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import android.preference.Preference.OnPreferenceChangeListener;
 
 import java.util.List;
 import java.util.ArrayList;
 
 import com.android.internal.logging.MetricsLogger;
 
-public class PdSettings extends SettingsPreferenceFragment  implements Indexable {
+public class PdSettings extends SettingsPreferenceFragment  implements OnPreferenceChangeListener, Indexable {
 
-private static final String TAG = "PdSettings";
+    private static final String TAG = "PdSettings";
+  
+    private static final String SHOW_FOURG = "show_fourg";
+    private static final String SHOW_THREEG = "show_threeg";
+    private SwitchPreference mShowFourG;
+    private SwitchPreference mShowThreeG;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.pd_settings);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver(); 
+
+        mShowFourG = (SwitchPreference) findPreference(SHOW_FOURG);
+        if (PDUtils.isWifiOnly(getActivity())) {
+            prefSet.removePreference(mShowFourG);
+        } else {
+            mShowFourG.setChecked((Settings.System.getInt(resolver,
+                Settings.System.SHOW_FOURG, 0) == 1));
+        }
+
+        mShowThreeG = (SwitchPreference) findPreference(SHOW_THREEG);
+        if (PDUtils.isWifiOnly(getActivity())) {
+            prefSet.removePreference(mShowThreeG);
+        } else {
+            mShowThreeG.setChecked((Settings.System.getInt(resolver,
+                Settings.System.SHOW_THREEG, 0) == 1));
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if  (preference == mShowFourG) {
+            boolean checked = ((SwitchPreference)preference).isChecked();
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.SHOW_FOURG, checked ? 1:0);
+            return true;
+        } else if  (preference == mShowThreeG) {
+            boolean checked = ((SwitchPreference)preference).isChecked();
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.SHOW_THREEG, checked ? 1:0);
+            return true;
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
     protected int getMetricsCategory()
     {
-	return MetricsLogger.APPLICATION;
+        return MetricsLogger.APPLICATION;
     }
-   public static final Indexable.SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Configuration config = getResources().getConfiguration(); 
+    }
+
+    public static final Indexable.SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
             new BaseSearchIndexProvider() {
                 @Override
                 public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
